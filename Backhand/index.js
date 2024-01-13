c3dc942b83a551e9d667d036210d7ec15bc86609
gabbarbhaiya15 <143590849+gabbarbhaiya15@users.noreply.github.com>
2024-01-13 11:50:06 +0530

    Update index.js

--- Backhand/index.js
+++ Backhand/index.js
@@ -48,11 +48,7 @@ app.use(cookieParser());
   .catch((err)=>{console.log(err)}) ;
 
 
-app.get("/",(req,res) =>{
-res.setHeader("Access-Control-Allow-Credentials","true");
-  res.send("api is running");
 
-});
 app.use('/register', signup);
 
 app.use('/login', Login);
